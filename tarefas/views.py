134ab241d6f0cc52000f2acf52d5aa0cd5958f97
Paulo Vitor <paulovitor.webdev@gmail.com>
2026-04-23 23:43:14 -0400

    feat: criação de tarefas com formulário POST

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tarefa
 
 def listar_tarefas(request):
     tarefas = Tarefa.objects.all()
     return render(request, 'tarefas/listar.html', {'tarefas': tarefas})
+
+def nova_tarefa(request):
+    if request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+
+        Tarefa.objects.create(titulo=titulo, descricao=descricao)
+
+        return redirect('listar_tarefas')
+    
+    return render(request, 'tarefas/nova.html')
