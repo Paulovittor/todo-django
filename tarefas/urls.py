a17edea29c05113a11f2e311ff5090eaaa0ae91b
Paulo Vitor <paulovitor.webdev@gmail.com>
2026-04-25 00:29:46 -0400

    feat: adição de template base, página de edição e modal de confirmação de exclusão

--- tarefas/urls.py
+++ tarefas/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_tarefas, name='listar_tarefas'),
-    path('nova/', views.nova_tarefa, name='nova_tarefa')
+    path('nova/', views.nova_tarefa, name='nova_tarefa'),
+    path('editar/<int:tarefa_id>/', views.editar_tarefa, name='editar_tarefa'),
+    path('deletar/<int:tarefa_id>/', views.deletar_tarefa, name='deletar_tarefa'),
 ]
